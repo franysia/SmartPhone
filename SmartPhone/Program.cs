99e7d63d1fca256e3466a0f42e89c91d03424dfc
franysia <francobocilff@gmail.com>
2026-04-17 10:26:13 +0700

    Menggunakan Abstraksi

--- SmartPhone/Program.cs
+++ SmartPhone/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ISmartPhone hpBudi = new Iphone();
+
+Fotografer fotografer = new Fotografer();
+fotografer.Kerja(hpBudi);
+
+hpBudi = new Samsung();
+fotografer.Kerja(hpBudi);
+interface ISmartPhone
+{
+    public void AmbilFoto();
+}
+
+class Iphone : ISmartPhone 
+{
+    public void AmbilFoto() 
+    {
+        Console.WriteLine("Iphone Mengaktifkan sensor kamera Prenium...*Cekrek");
+    }
+
+}
+
+class Samsung : ISmartPhone
+{
+    public void AmbilFoto() 
+    {
+        Console.WriteLine("Samsung Mengaktifkan Fitur Beauty...*Cekrek!*");
+    }
+}
+
+class Fotografer
+{
+    public void Kerja(ISmartPhone hp) 
+    {
+        hp.AmbilFoto();
+    }
+}
